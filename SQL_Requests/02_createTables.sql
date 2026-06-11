892f0a8836b8047cacca4559a2f6124ec222b83b
Gleb Dubas <147847299+Gilaneth@users.noreply.github.com>
2026-06-11 22:34:53 +0300

    updated 02_createTables.sql
    new table collections, associative table movie_in_collection, added id into table movie for IMDB, Rotten Tomatoes, Shikimori and Kinopoisk

--- SQL_Requests/02_createTables.sql
+++ SQL_Requests/02_createTables.sql
@@ -7,11 +7,13 @@ drop table if exists genre cascade;
 drop table if exists category cascade;
 drop table if exists movie_actor cascade;
 drop table if exists actor cascade;
+drop table if exists collections cascade;
+drop table if exists movie_in_collection cascade;
 
 drop type if exists watch_status;
 drop type if exists category_type;
 
-create type watch_status as enum ('watched', 'watching', 'planning');
+create type watch_status as enum ('watched', 'watching', 'planning', 'on_hold', 'dropped');
 create type category_type as enum ('movie', 'tv_series', 'anime', 'cartoon');
 
 create table users (
@@ -32,22 +34,31 @@ create table actor (
 
 create table movie (
 	movie_id serial primary key,
+	imdb_id varchar(20) null,
+	shikimori_id int null,
+	kinopoisk_id int null,
+	rottentomato_id varchar(255) null,
+	imdb_rating decimal null,
+	shikimori_rating decimal null,
+	rt_rating decimal null,
 	movie_name varchar(300) not null,
 	release_date date not null,
-	category category_type not null
+	category category_type not null,
+	cover_url varchar(500) null,
+	Description text
 );
 
 create table movie_genre (
-	movie_genre_id serial primary key,
 	movie_id int references movie(movie_id) on delete cascade,
-	genre_id int references genre(genre_id) on delete cascade
+	genre_id int references genre(genre_id) on delete cascade,
+	primary key(movie_id, genre_id)
 );
 
 
 create table movie_actor (
-	movie_actor_id serial primary key,
 	movie_id int references movie(movie_id) on delete cascade,
-	actor_id int references actor(actor_id) on delete cascade
+	actor_id int references actor(actor_id) on delete cascade,
+	primary key(movie_id, actor_id)
 );
 
 create table review (
@@ -66,6 +77,18 @@ create table watch_list (
 	added_at timestamp default current_timestamp not null,
 	user_id int references users(user_id) on delete cascade,
 	movie_id int references movie(movie_id) on delete cascade
+);
+
+create table collections (
+	collection_id serial primary key,
+	collection_name varchar(100) not null,
+	user_id int references users(user_id) on delete cascade
+);
+
+create table movie_in_collection (
+	movie_id int references movie(movie_id) on delete cascade,
+	collection_id int references collections(collection_id) on delete cascade,
+	primary key(movie_id, collection_id)
 );
 
 
