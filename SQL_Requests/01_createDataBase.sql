d1af2c5f53a701713ebe67d8f9c198fb0511bc01
eqoffical <eqSoftware@protonmail.com>
2026-06-11 13:22:59 +0300

    Updated gitignore, fixed locale in db creation

--- SQL_Requests/01_createDataBase.sql
+++ SQL_Requests/01_createDataBase.sql
@@ -2,9 +2,10 @@ CREATE DATABASE "watchDiaryDB"
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1251'
-    LC_CTYPE = 'English_United States.1251'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
     LOCALE_PROVIDER = 'libc'
+    TEMPLATE = template0
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
