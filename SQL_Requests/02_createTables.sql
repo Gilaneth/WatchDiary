682d44f8b101ba618ab6cca44191e2e97e949cdd
Vitalii <katrychvitalii23@gmail.com>
2026-06-28 13:36:43 +0300

    fix footer, nav authorization

---
+++ SQL_Requests/02_createTables.sql
@@ -0,0 +1,96 @@
+drop table if exists users cascade;
+drop table if exists review cascade;
+drop table if exists watch_list cascade;
+drop table if exists movie cascade;
+drop table if exists movie_genre cascade;
+drop table if exists genre cascade;
+drop table if exists category cascade;
+drop table if exists movie_actor cascade;
+drop table if exists actor cascade;
+drop table if exists collections cascade;
+drop table if exists movie_in_collection cascade;
+
+drop type if exists watch_status;
+drop type if exists category_type;
+
+create type watch_status as enum ('watched', 'watching', 'planning', 'on_hold', 'dropped');
+create type category_type as enum ('movie', 'tv_series', 'anime', 'cartoon');
+
+create table users (
+	user_id serial primary key,
+	username varchar(20) unique not null,
+	user_password varchar(255) not null,
+	user_email varchar(100) unique not null
+);
+
+create table genre (
+	genre_id serial primary key,
+	genre_name varchar(20) not null unique
+);
+
+create table actor (
+	actor_id serial primary key,
+	actor_name varchar(50) not null
+);
+
+
+create table movie (
+	movie_id serial primary key,
+	imdb_id varchar(20) null,
+	shikimori_id int null,
+	kinopoisk_id int null,
+	rottentomato_id varchar(255) null,
+	imdb_rating decimal null,
+	shikimori_rating decimal null,
+	rt_rating decimal null,
+	movie_name varchar(300) not null,
+	release_date date not null,
+	category category_type not null,
+	cover_url varchar(500) null,
+	Description text
+);
+
+create table movie_genre (
+	movie_id int references movie(movie_id) on delete cascade,
+	genre_id int references genre(genre_id) on delete cascade,
+	primary key(movie_id, genre_id)
+);
+
+
+create table movie_actor (
+	movie_id int references movie(movie_id) on delete cascade,
+	actor_id int references actor(actor_id) on delete cascade,
+	primary key(movie_id, actor_id)
+);
+
+create table review (
+	review_id serial primary key,
+	rating int not null check (rating >= 0 and rating <=10),
+	description text,
+	created_at timestamp default current_timestamp not null,
+	updated_at timestamp default current_timestamp,
+	user_id int references users(user_id) on delete cascade,
+	movie_id int references movie(movie_id) on delete cascade
+);
+
+create table watch_list (
+	watch_list_id serial primary key,
+	status watch_status not null,
+	added_at timestamp default current_timestamp not null,
+	user_id int references users(user_id) on delete cascade,
+	movie_id int references movie(movie_id) on delete cascade
+);
+
+create table collections (
+	collection_id serial primary key,
+	collection_name varchar(100) not null,
+	user_id int references users(user_id) on delete cascade
+);
+
+create table movie_in_collection (
+	movie_id int references movie(movie_id) on delete cascade,
+	collection_id int references collections(collection_id) on delete cascade,
+	primary key(movie_id, collection_id)
+);
+
+
---
+++ SQL_Requests/03_create_indexes.sql
@@ -0,0 +1,21 @@
+drop index if exists movie_actor_idx;
+drop index if exists movie_genre_idx;
+drop index if exists movie_collection_idx;
+drop index if exists watchlist_status_idx;
+drop index if exists review_idx;
+
+create index movie_actor_idx on movie_actor(actor_id);
+
+create index movie_genre_idx on movie_genre(genre_id);
+
+create index movie_collection_idx on movie_in_collection(collection_id);
+
+create index watchlist_status_idx on watchlist (user_id, status, added_at desc);
+
+create index review_idx on review (movie_id, updated_at desc);
+
+
+
+-- EXPLAIN ANALYZE SELECT * FROM watch_list WHERE user_id = 1; 
+
+analyze;
