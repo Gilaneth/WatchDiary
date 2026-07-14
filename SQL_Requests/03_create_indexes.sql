e8cc8edcfc5618753bb301eba873806b601a38b8
Gleb Dubas <147847299+Gilaneth@users.noreply.github.com>
2026-07-14 11:41:04 +0300

    Update 03_create_indexes.sql

--- SQL_Requests/03_create_indexes.sql
+++ SQL_Requests/03_create_indexes.sql
@@ -1,8 +1,8 @@
-drop index if exists movie_actor_idx;
-drop index if exists movie_genre_idx;
-drop index if exists movie_collection_idx;
-drop index if exists watchlist_status_idx;
-drop index if exists review_idx;
+drop index if exists movie_actor_idx cascade;
+drop index if exists movie_genre_idx cascade;
+drop index if exists movie_collection_idx cascade;
+drop index if exists watchlist_status_idx cascade;
+drop index if exists review_idx cascade;
 
 create index movie_actor_idx on movie_actor(actor_id);
 
@@ -10,7 +10,7 @@ create index movie_genre_idx on movie_genre(genre_id);
 
 create index movie_collection_idx on movie_in_collection(collection_id);
 
-create index watchlist_status_idx on watch_list (user_id, status, added_at desc);
+create index watchlist_status_idx on watch_list_item (user_id, status, added_at desc);
 
 create index review_idx on review (movie_id, updated_at desc);
 
